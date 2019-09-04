f9bc7c919ab2ae04b9771c722beca4b7b2abbb36
afriansyahian <afriansyah_i@rocketmail.com>
2019-09-04 08:40:08 +0700

    new update

---
+++ README.md/index.js
@@ -0,0 +1,44 @@
+//JavaScript
+
+const nameA = "Afriansyah";
+const nameB = "Ian";
+const nameCompared = nameA === nameB;
+
+console.log(nameA);
+console.log(nameB);
+console.log(nameCompared);
+
+const ageA = 24;
+const ageB = 25;
+
+if (ageA > ageB) {
+  console.log("Allowed to watch this move");
+} else if (ageA < ageB) {
+  console.log("not Allowed to watch this move");
+
+const ageA = 24;
+const ageB = 25;
+
+  if (ageA < ageB) {
+    console.log("ke log lg");
+  }
+} else {
+  console.log("you bring your parent");
+}
+
+const personA = {
+  name: "Afriansyah",
+  age: 20
+};
+
+const personB = {
+  name: "Betty",
+  age: 30
+}
+switch (personA.age < personB.age){
+  case true:
+    console.log("age falid")
+    break
+}
+
+// switch (personalA.age > )
